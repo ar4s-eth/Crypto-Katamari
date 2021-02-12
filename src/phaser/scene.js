d83bf61d408b559ba4be45ff763071ffff8d9a71
Ashley <ashley.barr@ar4s.com>
2021-02-11 22:25:04 -0700

    fix images loading

--- src/phaser/scene.js
+++ src/phaser/scene.js
@@ -1,5 +1,5 @@
 import Phaser from "phaser";
-import logoImg from "../assets/logo.png";
+// import logoImg from "assets/logo.png";
 
 
 // OG Code
@@ -39,12 +39,12 @@ class PlayGame extends Phaser.Scene {
 
   preload() {
     
-    this.game.load.image('eye', '../assets/sprites/eye.png');
-    this.game.load.image('body', '../assets/sprites/body.png');
-    this.game.load.image('arm-l', '../assets/sprites/arm-l.png');
-    this.game.load.image('arm-r', '../assets/sprites/arm-r.png');
-    this.game.load.image('leg-l', '../assets/sprites/leg-l.png');
-    this.game.load.image('leg-r', '../assets/sprites/leg-r.png');
+    this.load.image('eye', 'src/assets/sprites/eye.png');
+    this.load.image('body', 'src/assets/sprites/body.png');
+    this.load.image('arm-l', 'src/assets/sprites/arm-l.png');
+    this.load.image('arm-r', 'src/assets/sprites/arm-r.png');
+    this.load.image('leg-l', 'src/assets/sprites/leg-l.png');
+    this.load.image('leg-r', 'src/assets/sprites/leg-r.png');
   } 
   
   create() {
@@ -52,13 +52,14 @@ class PlayGame extends Phaser.Scene {
     // Use groups of sprites to create a big this.robot.
     // Robot itself, you can subclass group class in a real this.game.
 
-    this.robot = this.game.add.group();
+    this.robot = this.add.group();
     
     this.robot.x = 300;
     this.robot.y = 200;
     
-    this.robot.pivot.x = 300;
-    this.robot.pivot.y = 300;
+    // this.robot.pivot.x = 300;
+    // this.robot.pivot.y = 300;
+    
 
   // Robot components.
   this.robot.create(90, 175, 'arm-l');
@@ -70,13 +71,14 @@ class PlayGame extends Phaser.Scene {
   }
   
   update() {
-
-  this.robot.rotation += 0.02;
+    Phaser.Actions.RotateAroundDistance(this.robot.getChildren(), { x: 300, y: 300 }, 0.02, 200)
+    // this.robot.setAnchor(10.5, 0.5)
+  // this.robot.rotation += 0.02;
   }
   
   render() {
   
-  this.game.debug.text('The robot is a group and every component is a sprite.', 240, 580);
+  this.debug.text('The robot is a group and every component is a sprite.', 240, 580);
   }
 }
 
