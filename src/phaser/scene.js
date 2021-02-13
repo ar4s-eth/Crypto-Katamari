1432ee2d562ea5219a29b9a2d1d8188e0e139e50
Ashley <ashley.barr@ar4s.com>
2021-02-12 17:25:04 -0700

    add katamari && nfts

--- src/phaser/scene.js
+++ src/phaser/scene.js
@@ -33,47 +33,90 @@ class PlayGame extends Phaser.Scene {
   
   constructor() {
     super('PlayGame');
-    this.robot = {};
   }
   
 
   preload() {
     
-    this.load.image('eye', 'src/assets/sprites/eye.png');
-    this.load.image('body', 'src/assets/sprites/body.png');
-    this.load.image('arm-l', 'src/assets/sprites/arm-l.png');
-    this.load.image('arm-r', 'src/assets/sprites/arm-r.png');
-    this.load.image('leg-l', 'src/assets/sprites/leg-l.png');
-    this.load.image('leg-r', 'src/assets/sprites/leg-r.png');
+    this.load.image('kball', 'src/assets/sprites/kball.png');
+    this.load.image('nft1', 'src/assets/sprites/img_nft1.png');
+    this.load.image('nft2', 'src/assets/sprites/img_nft2.png');
+    this.load.image('nft3', 'src/assets/sprites/img_nft3.jpg');
+    this.load.image('ntf4', 'src/assets/sprites/img_nft4.png');
+    this.load.image('ntf5', 'src/assets/sprites/gif_nft.gif');
   } 
   
   create() {
-    
-    // Use groups of sprites to create a big this.robot.
-    // Robot itself, you can subclass group class in a real this.game.
 
-    this.robot = this.add.group();
+    // Initialize the katamari Object
+    this.katamari
+  
+
+    // Create the katamari container && size it
+    this.katamari = this.add.container(500, 400)
+    this.katamari.setSize(600, 600)
+
+    // Load the kball
+    this.kball = this.add.image(0, 0, 'kball');
+
+    // Add kball to Katamari container
+    this.katamari.add(this.kball)
+
+    // Scale the kball to fit the container
+    this.kball.setScale(0.2)
+
+    // Create orbiting nft's
+    this.nft1 = this.add.sprite(0, 50, 'nft1')
+    this.nft1.setScale(0.5)
+
+    this.nft2 = this.add.sprite(50, 0, 'nft2')
+    this.nft2.setScale(0.1)
+
+    this.nft3 = this.add.sprite(-80, -50, 'nft3')
+    this.nft3.setScale(0.1)
+
+    this.nft5 = this.add.sprite(200, 200, 'nft5')
+    this.nft5.setScale(1)
+
+    //Add them to the Katamari
+    this.katamari.add(this.nft1)
+    this.katamari.add(this.nft2)
+    this.katamari.add(this.nft3)
+    this.katamari.add(this.nft5)
+
+    /* Features to Add
     
-    this.robot.x = 300;
-    this.robot.y = 200;
-    
-    // this.robot.pivot.x = 300;
-    // this.robot.pivot.y = 300;
+    *SIZING*
+
+    -Dynamically size Katamari based on the radius of the container
+    --Dynamically size NFT's based on the size of the catamari
+
+    *DATA*
+    -Take in NFT's as an array of objects
+    --Have their positions assigned randomly but accordingly
+    --Choose metadata to include, find a way to include it for hover
+    ---Include Hyperlinks to click on
+
+    *BEHAVIOR*
+    -Speed up & slow down
+    -When NFT's outside of the container touch the radius, include them at the coordinates they touch at.
+
+*/
     
 
-  // Robot components.
-  this.robot.create(90, 175, 'arm-l');
-  this.robot.create(549, 175, 'arm-r');
-  this.robot.create(270, 325, 'leg-l');
-  this.robot.create(410, 325, 'leg-r');
-  this.robot.create(219, 32, 'body');
-  this.robot.create(335, 173,'eye');
+    console.log(this.kball)
+    
+
   }
   
   update() {
-    Phaser.Actions.RotateAroundDistance(this.robot.getChildren(), { x: 300, y: 300 }, 0.02, 200)
-    // this.robot.setAnchor(10.5, 0.5)
-  // this.robot.rotation += 0.02;
+    // Phaser.Actions.RotateAroundDistance(this.kball.getChildren(), { x: 500, y: 400 }, 0.02, 200)
+
+    this.katamari.rotation += 0.01
+    // this.sprite.setScale(ratio, .3)
+    
+    // this.kball.setAnchor(10.5, 0.5)
+  // this.kball.rotation += 0.02;
   }
   
   render() {
