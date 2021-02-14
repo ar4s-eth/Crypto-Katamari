094ce92b9fbb0e5aa57f4548501cc87f714a5499
Ashley <ashley.barr@ar4s.com>
2021-02-14 10:44:26 -0700

    add rolling katamari

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,19 @@
 import React from "react";
 import Phaser from "phaser";
-import World from "../phaser/world"
-import PlayGame from "../phaser/scene";
+import World from "../scene/world"
+import Katamari from "../scene/katamari";
 
 export const config = {
 	type: Phaser.AUTO,
 	parent: "phaser",
 	width: 1024,
 	height: 768,
-	scene: [
-		new World({ key: 'world'}),
-		new PlayGame({ key: 'play'})
-	],
-	// scene: PlayGame,
-	// loader: { baseURL:}
-	callbacks: {
-    postBoot: function (play) {
-      // Don't use this anywhere else ;)
-      // Check that your keys are correct.
-      // 'boot' should be loading (active), 'game' and 'hud' should be init (inactive)
-      play.scene.dump();
-		}
-	}
+	scene: [ World ]
 };
 
 const game = new Phaser.Game(config);
 
-console.log(game)
+// console.log(game)
 
 export default function App(props) {	
 	return <div>{game.canvas}</div>
