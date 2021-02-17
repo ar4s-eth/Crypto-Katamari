bffd9e7b1f8e02a5deebe9aa85341cbf78f4ec40
Ashley <ashley.barr@ar4s.com>
2021-02-16 19:41:20 -0700

    add promise handling

--- src/helpers/convert.js
+++ src/helpers/convert.js
@@ -1,9 +1,8 @@
-
-
 const weiToEthUsd = (wei, usdBool) => {
   const usd = 1808.3
   const eth = wei * 0.000000000000000001;
   return usdBool ? `$${(eth * usd).toFixed(2)} USD` : `${eth} ETH`;
 };
 
-module.exports = weiToEthUsd
+module.exports = { weiToEthUsd }
+
--- src/helpers/getPoolData.js
+++ src/helpers/getPoolData.js
@@ -18,7 +18,7 @@ const queryPrizeGross = `query {
 
 // takes two arguments when called, for the URL of the 
 // API and the query you want to send. I can always change this.
-export function callPoolTogetherApi(url, query) {
+function callPoolTogetherApi(url, query) {
   return axios({
     url: url,
     method: 'post',
@@ -29,7 +29,7 @@ export function callPoolTogetherApi(url, query) {
     /* Possible to just return res.data and handle the nesting when it's
     being used. That will make this funciton more general but a bit more difficult
     to use in production? We should talk about this */
-    return res.data.data.prizePools[0];
+    return res.data.data.prizePools[0] ;
     // The return statement in that case would look as follows:
     // console.log(res.data.data);
   }).catch(err => console.error(err));
@@ -37,4 +37,4 @@ export function callPoolTogetherApi(url, query) {
 
 const poolData = callPoolTogetherApi(poolTogetherGraph, queryPrizeGross)
 
-module.exports = poolData;
+module.exports = { poolData };
