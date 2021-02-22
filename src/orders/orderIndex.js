957def6e67835b80d5ab33c8449ddcbd9053730a
Johnny Prencipe <johnny.prencipe@gmail.com>
2021-02-21 16:07:03 -0800

    Add index file for orders

---
+++ src/orders/orderIndex.js
@@ -0,0 +1,382 @@
+const order0 = require('./order0');
+const order1 = require('./order1');
+const order2 = require('./order2');
+const order3 = require('./order3');
+const order5 = require('./order5');
+const order6 = require('./order6');
+const order7 = require('./order7');
+const order9 = require('./order9');
+const order10 = require('./order10');
+const order11 = require('./order11');
+const order12 = require('./order12');
+const order13 = require('./order13');
+const order14 = require('./order14');
+const order15 = require('./order15');
+const order16 = require('./order16');
+const order17 = require('./order17');
+const order18 = require('./order18');
+const order19 = require('./order19');
+const order20 = require('./order20');
+const order21 = require('./order21');
+const order22 = require('./order22');
+const order23 = require('./order23');
+const order24 = require('./order24');
+const order25 = require('./order25');
+const order26 = require('./order26');
+const order27 = require('./order27');
+const order28 = require('./order28');
+const order29 = require('./order29');
+const order30 = require('./order30');
+const order31 = require('./order31');
+const order32 = require('./order32');
+const order33 = require('./order33');
+const order34 = require('./order34');
+const order35 = require('./order35');
+const order36 = require('./order36');
+const order37 = require('./order37');
+const order38 = require('./order38');
+const order39 = require('./order39');
+const order40 = require('./order40');
+const order41 = require('./order41');
+const order42 = require('./order42');
+const order43 = require('./order43');
+const order44 = require('./order44');
+const order45 = require('./order45');
+const order46 = require('./order46');
+const order47 = require('./order47');
+const order48 = require('./order48');
+const order49 = require('./order49');
+const order50 = require('./order50');
+const order51 = require('./order51');
+const order52 = require('./order52');
+const order53 = require('./order53');
+const order54 = require('./order54');
+const order55 = require('./order55');
+const order56 = require('./order56');
+const order57 = require('./order57');
+const order58 = require('./order58');
+const order59 = require('./order59');
+const order60 = require('./order60');
+const order61 = require('./order61');
+const order62 = require('./order62');
+const order63 = require('./order63');
+const order64 = require('./order64');
+const order65 = require('./order65');
+const order66 = require('./order66');
+const order67 = require('./order67');
+const order68 = require('./order68');
+const order69 = require('./order69');
+const order70 = require('./order70');
+const order71 = require('./order71');
+const order72 = require('./order72');
+const order73 = require('./order73');
+const order74 = require('./order74');
+const order75 = require('./order75');
+const order76 = require('./order76');
+const order77 = require('./order77');
+const order78 = require('./order78');
+const order79 = require('./order79');
+const order80 = require('./order80');
+const order81 = require('./order81');
+const order82 = require('./order82');
+const order83 = require('./order83');
+const order84 = require('./order84');
+const order85 = require('./order85');
+const order86 = require('./order86');
+const order87 = require('./order87');
+const order88 = require('./order88');
+const order89 = require('./order89');
+const order90 = require('./order90');
+const order91 = require('./order91');
+const order92 = require('./order92');
+const order93 = require('./order93');
+const order94 = require('./order94');
+const order95 = require('./order95');
+const order96 = require('./order96');
+const order98 = require('./order98');
+const order99 = require('./order99');
+const order100 = require('./order100');
+const order101 = require('./order101');
+const order102 = require('./order102');
+const order103 = require('./order103');
+const order104 = require('./order104');
+const order105 = require('./order105');
+const order106 = require('./order106');
+const order107 = require('./order107');
+const order108 = require('./order108');
+const order109 = require('./order109');
+const order110 = require('./order110');
+const order111 = require('./order111');
+const order112 = require('./order112');
+const order113 = require('./order113');
+const order114 = require('./order114');
+const order115 = require('./order115');
+const order116 = require('./order116');
+const order117 = require('./order117');
+const order119 = require('./order119');
+const order120 = require('./order120');
+const order121 = require('./order121');
+const order122 = require('./order122');
+const order123 = require('./order123');
+const order124 = require('./order124');
+const order125 = require('./order125');
+const order126 = require('./order126');
+const order127 = require('./order127');
+const order128 = require('./order128');
+const order129 = require('./order129');
+const order130 = require('./order130');
+const order131 = require('./order131');
+const order132 = require('./order132');
+const order133 = require('./order133');
+const order134 = require('./order134');
+const order135 = require('./order135');
+const order137 = require('./order137');
+const order138 = require('./order138');
+const order139 = require('./order139');
+const order140 = require('./order140');
+const order141 = require('./order141');
+const order142 = require('./order142');
+const order143 = require('./order143');
+const order145 = require('./order145');
+const order146 = require('./order146');
+const order147 = require('./order147');
+const order148 = require('./order148');
+const order149 = require('./order149');
+const order150 = require('./order150');
+const order151 = require('./order151');
+const order152 = require('./order152');
+const order153 = require('./order153');
+const order154 = require('./order154');
+const order155 = require('./order155');
+const order156 = require('./order156');
+const order157 = require('./order157');
+const order158 = require('./order158');
+const order159 = require('./order159');
+const order160 = require('./order160');
+const order161 = require('./order161');
+const order162 = require('./order162');
+const order163 = require('./order163');
+const order164 = require('./order164');
+const order165 = require('./order165');
+const order166 = require('./order166');
+const order167 = require('./order167');
+const order168 = require('./order168');
+const order169 = require('./order169');
+const order170 = require('./order170');
+const order172 = require('./order172');
+const order173 = require('./order173');
+const order175 = require('./order175');
+const order176 = require('./order176');
+const order177 = require('./order177');
+const order179 = require('./order179');
+const order180 = require('./order180');
+const order181 = require('./order181');
+const order182 = require('./order182');
+const order183 = require('./order183');
+const order184 = require('./order184');
+const order187 = require('./order187');
+const order189 = require('./order189');
+const order190 = require('./order190');
+const order191 = require('./order191');
+const order192 = require('./order192');
+const order193 = require('./order193');
+const order194 = require('./order194');
+const order195 = require('./order195');
+const order196 = require('./order196');
+const order197 = require('./order197');
+const order198 = require('./order198');
+const order199 = require('./order199');
+
+//###########################################
+
+
+module.exports = {
+  order0,
+  order1,
+  order2,
+  order3,
+  order5,
+  order6,
+  order7,
+  order9,
+  order10,
+  order11,
+  order12,
+  order13,
+  order14,
+  order15,
+  order16,
+  order17,
+  order18,
+  order19,
+  order20,
+  order21,
+  order22,
+  order23,
+  order24,
+  order25,
+  order26,
+  order27,
+  order28,
+  order29,
+  order30,
+  order31,
+  order32,
+  order33,
+  order34,
+  order35,
+  order36,
+  order37,
+  order38,
+  order39,
+  order40,
+  order41,
+  order42,
+  order43,
+  order44,
+  order45,
+  order46,
+  order47,
+  order48,
+  order49,
+  order50,
+  order51,
+  order52,
+  order53,
+  order54,
+  order55,
+  order56,
+  order57,
+  order58,
+  order59,
+  order60,
+  order61,
+  order62,
+  order63,
+  order64,
+  order65,
+  order66,
+  order67,
+  order68,
+  order69,
+  order70,
+  order71,
+  order72,
+  order73,
+  order74,
+  order75,
+  order76,
+  order77,
+  order78,
+  order79,
+  order80,
+  order81,
+  order82,
+  order83,
+  order84,
+  order85,
+  order86,
+  order87,
+  order88,
+  order89,
+  order90,
+  order91,
+  order92,
+  order93,
+  order94,
+  order95,
+  order96,
+  order98,
+  order99,
+  order100,
+  order101,
+  order102,
+  order103,
+  order104,
+  order105,
+  order106,
+  order107,
+  order108,
+  order109,
+  order110,
+  order111,
+  order112,
+  order113,
+  order114,
+  order115,
+  order116,
+  order117,
+  order119,
+  order120,
+  order121,
+  order122,
+  order123,
+  order124,
+  order125,
+  order126,
+  order127,
+  order128,
+  order129,
+  order130,
+  order131,
+  order132,
+  order133,
+  order134,
+  order135,
+  order137,
+  order138,
+  order139,
+  order140,
+  order141,
+  order142,
+  order143,
+  order145,
+  order146,
+  order147,
+  order148,
+  order149,
+  order150,
+  order151,
+  order152,
+  order153,
+  order154,
+  order155,
+  order156,
+  order157,
+  order158,
+  order159,
+  order160,
+  order161,
+  order162,
+  order163,
+  order164,
+  order165,
+  order166,
+  order167,
+  order168,
+  order169,
+  order170,
+  order172,
+  order173,
+  order175,
+  order176,
+  order177,
+  order179,
+  order180,
+  order181,
+  order182,
+  order183,
+  order184,
+  order187,
+  order189,
+  order190,
+  order191,
+  order192,
+  order193,
+  order194,
+  order195,
+  order196,
+  order197,
+  order198,
+  order199
+}
