ccf0a6e04fb3f0639e0b74e50a95aa8a86fac647
Ashley <ashley.barr@ar4s.com>
2021-02-23 15:56:42 -0700

    fix orderincrementer

--- src/scene/world.js
+++ src/scene/world.js
@@ -274,22 +274,14 @@ export default class World extends Phaser.Scene {
       this.load.start()
     }
 
-    let foo = 0
+    let orderIncrementer = 0
     setInterval(function(){
-<<<<<<< HEAD
-      // console.log(ordersArray[0]);
-      // console.log(foo)
-      loadNFT(ordersArray[foo]);
-      foo++
-    }, 5000);
-=======
       orderIncrementer === ordersArray.length ?
       orderIncrementer = 0 :
         console.log(`Order ${orderIncrementer} of ${ordersArray.length}`)
         loadNFT(ordersArray[orderIncrementer]);
         orderIncrementer++;
     }, 500);
->>>>>>> 84c2ef5d2d166a78b5d55f664756e1293dd0d7c7
     
       // this.katamari.fixedToCamera(cam)
       // this.cursors = this.input.keyboard.createCursorKeys();
