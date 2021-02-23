c0ed5c5a50ceebd1e2e406a305af99bbbce42861
Johnny Prencipe <johnny.prencipe@gmail.com>
2021-02-22 17:32:35 -0800

    Add orders as array

---
+++ src/orders/ordersArray.js
@@ -0,0 +1,20 @@
+const orders = require('./orderIndex');
+const { weiToEthUsd } = require('../helpers/convertWei')
+
+const ordersArray = [];
+for (const order in orders) {
+
+  if (orders[order][order].asset) {
+    ordersArray.push({ 
+      ethPrice: weiToEthUsd(orders[order][order].current_price, 0),
+      usdPrice: weiToEthUsd(orders[order][order].current_price, 1),
+      image_thumbnail: orders[order][order].asset.image_thumbnail_url,
+      image_full: orders[order][order].asset.image_url,
+      name: orders[order][order].asset.name,
+      description: orders[order][order].asset.description
+    });
+  }
+
+}
+
+module.exports = { ordersArray };
