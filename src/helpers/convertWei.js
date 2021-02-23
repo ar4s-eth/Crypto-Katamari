321e469c904dda1fef7947ff1473020aee3941a9
Johnny Prencipe <johnny.prencipe@gmail.com>
2021-02-23 12:33:33 -0800

    Update USD, try to add live updates

--- src/helpers/convertWei.js
+++ src/helpers/convertWei.js
@@ -1,6 +1,19 @@
+// const axios = require('axios');
+// axios.get('https://api.etherscan.io/api?module=stats&action=ethprice&apikey=APIKEY')
+// .then(res => {
+//   if (res.status === 1) {
+//     usd = res.result.ethusd;
+//   } else {
+//     usd = 1500;
+//   }
+//   // const eth = wei * 0.000000000000000001;
+//   // return usdBool ? `$${(eth * usd).toFixed(2)}` : `${eth.toFixed(3)}`;
+// });
+
 const weiToEthUsd = (wei, usdBool) => {
-  const usd = 1808.3
   const eth = wei * 0.000000000000000001;
+  const usd = 1500;
+
   return usdBool ? `$${(eth * usd).toFixed(2)}` : `${eth.toFixed(3)}`;
 };
 
